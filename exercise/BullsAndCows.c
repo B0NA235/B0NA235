b7897215a632a1779a8ac0b488e28faa45cf82c5
B0NA235 <ohbona715@gmail.com>
2025-10-05 06:38:35 +0000

    BullsAndCows

---
+++ exercise/BullsAndCows.c
@@ -0,0 +1,18 @@
+//BullsAndCows.c
+/*4자리 숫자를임의로 정한 뒤 플레이어가 예측 숫자를 부른다(최대 9번)
+사용되는 숫자는 0에서 9까지 서로 다른 숫자
+숫자는 맞지만 위치가 틀렸으면 BALL
+숫자와 위치 전부 맞으면 STRIKE
+숫자 위치 전부 틀리면 OUT
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+    while(1)
+    {
+
+    }
+}
